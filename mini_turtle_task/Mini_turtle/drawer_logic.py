300a3bba999ed9b2ebc424ab43f662234500352f
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-12-10 16:23:47 -0500

    Descripciones codigo 2

--- mini_turtle_task/Mini_turtle/drawer_logic.py
+++ mini_turtle_task/Mini_turtle/drawer_logic.py
@@ -8,11 +8,11 @@ def adelante(pasos): #  función para mover hacia adelante
 
 def abajo(pasos):  # función para mover hacia abajo
     global posicion_x
-    for _ in range(pasos):
-        print(" " * posicion_x + "|")
-    print(" " * posicion_x + "v")
+    for _ in range(pasos): 
+        print(" " * posicion_x + "|") # Dibuja la línea vertical hacia abajo
+    print(" " * posicion_x + "v")  # Dibuja la flecha hacia abajo
 
     
-def reiniciar():   
+def reiniciar():   # función para reiniciar la posición
     global posicion_x
-    posicion_x = 0
+    posicion_x = 0 # Reinicia la posición a cero
