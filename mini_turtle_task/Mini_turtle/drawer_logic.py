4fa9f2c0c867610cc1227d44e3a126783884c6d3
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-12-10 16:12:56 -0500

    Funcion Abajo

--- mini_turtle_task/Mini_turtle/drawer_logic.py
+++ mini_turtle_task/Mini_turtle/drawer_logic.py
@@ -5,3 +5,9 @@ def adelante(pasos):
     global posicion_x
     print(" " * posicion_x + "-" * pasos + ">")
     posicion_x += pasos
+
+def abajo(pasos):
+    global posicion_x
+    for _ in range(pasos):
+        print(" " * posicion_x + "|")
+    print(" " * posicion_x + "v")
