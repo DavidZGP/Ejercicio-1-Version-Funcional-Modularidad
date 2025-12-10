430e37bf82d285b2ac886170ef736db867cc645e
DavidZGP <david.piedrahitap@est.iudigital.edu.co>
2025-12-10 16:43:17 -0500

    Pruebas Adicionales Main

--- mini_turtle_task/Main.py
+++ mini_turtle_task/Main.py
@@ -4,3 +4,15 @@ from mini_turtle import adelante, abajo, reiniciar
 adelante(5)
 abajo(3)
 adelante(2)
+
+reiniciar()
+
+abajo(2)
+adelante(4)
+
+reiniciar()
+
+abajo(3)
+adelante(3)
+abajo(2)
+adelante(6)
